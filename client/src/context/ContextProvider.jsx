fbfd10888e7fbad663746de3866b4aa9422e7075
vivekratr <vivekratr@gmail.com>
2024-05-26 03:55:10 +0530

    chore: Add getEthereumContract function to ContextProvider.jsx

--- client/src/context/ContextProvider.jsx
+++ client/src/context/ContextProvider.jsx
@@ -3,3 +3,21 @@ import { ethers,utils } from 'ethers';
 import { contractABI, contractAddress } from "../utils/constants";
 
 export const Context = React.createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const socoinContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
+  console.log("ethereum", ethereum);
+  console.log(contractABI, contractAddress);
+  console.log("signer", signer);
+  console.log("provider", provider);
+  console.log("Contract", socoinContract);
+  return socoinContract;
+};
