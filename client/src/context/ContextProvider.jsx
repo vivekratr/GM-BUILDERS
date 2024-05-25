c5f856078dda8263e25ebdaa7f49413c2c50de1c
vivekratr <vivekratr@gmail.com>
2024-05-26 04:12:39 +0530

    feat: Add handleProfileDataReturned function to handle profile data in ContextProvider.jsx

--- client/src/context/ContextProvider.jsx
+++ client/src/context/ContextProvider.jsx
@@ -22,6 +22,80 @@ const getEthereumContract = () => {
   return socoinContract;
 };
 export const BlogProvider = ({ children }) => {
+    const handleProfileDataReturned  = (data) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("dapp receives:", receivedData);
+        setData(receivedData.data[0].assetData)
+        // alert(JSON.stringify(receivedData));
+        childRef.current.closeSocialConnectPopup();
+    };
+     // Web3 function handles
+     const handleGetAllAccounts = (data ) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("Get all accounts:", receivedData);
+        alert(JSON.stringify(data));
+    };
+    const handleGetConnectedAccount = (data ) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("Get connected account:", receivedData);
+        alert(JSON.stringify(data));
+    };
+    const handleMessageSignature = (data ) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("Get message signature:", receivedData);
+        alert(JSON.stringify(data));
+    };
+    const handleVerifyMessageSignature = (data ) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("Verify message signature:", receivedData);
+        alert(JSON.stringify(data));
+    };
+    const handleGetBalance = (data ) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("Get balance:", receivedData);
+        alert(JSON.stringify(data));
+    };
+    const handleSendTransaction = (data ) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("Send transaction:", receivedData);
+        alert(JSON.stringify(data));
+    };
+    const handleGetBlockNumber = (data ) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("Get block number:", receivedData);
+        alert(JSON.stringify(data));
+    };
+    const handleGetTransactionCount = (data ) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("Get transaction count:", receivedData);
+        alert(JSON.stringify(data));
+    };
+    const handleReadFromContract = (data ) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("Read from contract:", receivedData);
+        alert(JSON.stringify(data));
+    };
+    const handleWriteToContract = (data ) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("Write to contract:", receivedData);
+        alert(JSON.stringify(data));
+    };
+    const handleErrorMessage = (data ) => {
+        const receivedData = JSON.parse(JSON.stringify(data))
+        console.log("Get error message:", receivedData);
+        alert(JSON.stringify(data));
+    };
+
+
+    const renderData = (data) => {
+      if (typeof data === 'object' && data !== null) {
+          return (
+              <pre>{JSON.stringify(data, null, 2)}</pre>
+          );
+      } else {
+          return <div>{data}</div>;
+      }
+  };
 return (
     <Context.Provider
       value={{ }}
