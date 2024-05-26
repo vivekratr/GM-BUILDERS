ac24705b5d98f2016f4ac87f5feac0d6bd8ae625
vivekratr <vivekratr@gmail.com>
2024-05-26 21:50:23 +0530

    Refactor toggle buttons and styling in BlogSection page

--- client/src/Pages/BlogSection.jsx
+++ client/src/Pages/BlogSection.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import "./css/BlogSection.css";
+import BackgroundImageDiv from "../components/BGImageDiv";
 
 const BlogSection = () => {
   const [activeButton, setActiveButton] = useState(null);
@@ -142,7 +143,28 @@ const BlogSection = () => {
       <div
         style={{ gridTemplateRows: `530px  auto` }}
         className="grid grid-cols-12 bg-black pb-[150px]   gap-x-[25px] gap-y-[33px] w-[1200px] mx-auto p-1 "
-      ></div>
+      >
+
+         {/* 1st column */}
+      <div className="relative col-span-4">
+        <div className="w-full relative [background:linear-gradient(180deg,_rgba(0,_0,_0,_0),_#000_50.9%)] h-[15.563rem]" />
+        <BackgroundImageDiv
+          classs={
+            "border-[1px] border-solid border-[#3b3b3b] px-7 flex items-center justify-start "
+          }
+          imageUrl={`https://i.imgur.com/n9yeXKZ.png`}
+        >
+          <div className="w-[240px] bottom-12 flex items-center justify-center relative bg-[#FFFFFF7A] box-border h-[3.625rem] overflow-hidden text-left text-[1.5rem] text-white font-inter border-[1px] border-solid border-white">
+            <div className=" capitalize font-semibold">Get started</div>
+            <img
+              className=" w-[1.656rem] h-[0.781rem]"
+              alt=""
+              src="https://i.imgur.com/CI4EIog.png"
+            />
+          </div>
+        </BackgroundImageDiv>
+      </div>
+      </div>
       {/* cards */}
     </div>
   );
