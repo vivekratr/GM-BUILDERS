40a5b5c086ec775599ac273e76b18ea3e4697324
vivekratr <vivekratr@gmail.com>
2024-05-26 11:19:24 +0530

    Refactor Footer component to remove unused code

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,8 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default Footer
+export default Footer;
