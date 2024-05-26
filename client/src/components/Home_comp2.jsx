c73fde067bc5272944a93372e445cf0a638790ee
vivekratr <vivekratr@gmail.com>
2024-05-26 07:50:05 +0530

    chore: Update Home_comp2 component formatting and grid layout

--- client/src/components/Home_comp2.jsx
+++ client/src/components/Home_comp2.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home_comp2 = () => {
   return <>
-  <div>
+  <div style={{ gridTemplateRows: `260px 360px 314px auto`}} className="grid grid-cols-12 gap-[25px] w-[97%] mx-auto p-1 ">
 
   </div>
   </>
