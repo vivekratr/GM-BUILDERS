d1d503a7fad9ed8e88915ddc8cfb68b3099040a2
vivekratr <vivekratr@gmail.com>
2024-05-27 00:09:04 +0530

    Refactor ViewBlog component

--- client/src/Pages/ViewBlog.jsx
+++ client/src/Pages/ViewBlog.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const ViewBlog = () => {
-  return <div></div>;
+  return <div>
+    {/* navbar */}
+    <div>
+
+        <div>
+            <img src="" alt="" />
+        </div>
+
+        <div>
+            
+        </div>
+    </div>
+  </div>;
 };
 
 export default ViewBlog;
