0860a3ae93e0399a3aeaca76f3d236383314aea5
vivekratr <vivekratr@gmail.com>
2024-05-26 10:38:21 +0530

    Refactor Home_comp5 component to update background image styling and improve grid layout

--- client/src/components/Home_comp5.jsx
+++ client/src/components/Home_comp5.jsx
@@ -1,7 +1,14 @@
 // import React from 'react'
 
+import BackgroundImageDiv from "./BGImageDiv";
+
 const Home_comp5 = () => {
-  return <div></div>;
+  return <div className=" bg-black px-11 mx-auto w-full">
+    <BackgroundImageDiv classs={` flex items-start justify-center p-[30px]`} imageUrl={``} >
+
+    </BackgroundImageDiv>
+
+  </div>;
 };
 
 export default Home_comp5;
