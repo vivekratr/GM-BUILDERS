881402570649a1f6fab9f15a58b5c38709430848
vivekratr <vivekratr@gmail.com>
2024-05-26 20:56:22 +0530

    chore: Add BlogSection page and update routing in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { BlogProvider } from "./context/ContextProvider";
 import Home from "./Pages/Home";
+import BlogSection from "./Pages/BlogSection";
 
 function App() {
   return  <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
     <BlogProvider>
       <Routes>
         <Route path="/" element={<Home /*state = {state}*/ />} />
+        <Route path="/blog" element={<BlogSection /*state = {state}*/ />} />
         </Routes>
           {/* {<RegisterPage/>}  */}
         </BlogProvider>
--- client/src/Pages/BlogSection.jsx
+++ client/src/Pages/BlogSection.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Navbar from "../components/Navbar";
 
 const BlogSection = () => {
   return (
     <div>
-      
+        <div>
+
+      <Navbar/>
+        </div>
     </div>
   )
 }
