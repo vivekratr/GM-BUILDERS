f9fc81f13d6fbe6cac096015d5fa7912a470c702
vivekratr <vivekratr@gmail.com>
2024-05-26 11:24:31 +0530

    Refactor Footer component to add image in the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,10 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return <div className=" flex justify-center items-start ">
+ <img src="https://i.imgur.com/ARpUAx4.png" alt="" className="w-[537px] h-[521px] object-cover" />
+ <img src="https://i.imgur.com/lxaS5XJ.png" alt="" className="object-cover" />
+  </div>;
 };
 
 export default Footer;
