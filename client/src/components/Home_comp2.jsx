2fc6424f56aaa7bdcc4b8eb367dd9ece2aa466aa
vivekratr <vivekratr@gmail.com>
2024-05-26 07:52:17 +0530

    chore: Update Home_comp2 component formatting and add missing element

--- client/src/components/Home_comp2.jsx
+++ client/src/components/Home_comp2.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 const Home_comp2 = () => {
-  return <>
-  <div style={{ gridTemplateRows: `260px 360px 314px auto`}} className="grid grid-cols-12 gap-[25px] w-[97%] mx-auto p-1 ">
-
-  </div>
-  </>
+  return (
+    <>
+      <div
+        style={{ gridTemplateRows: `260px 360px 314px auto` }}
+        className="grid grid-cols-12 gap-[25px] w-[97%] mx-auto p-1 "
+      >
+        {/* 1st row */}
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default Home_comp2;
