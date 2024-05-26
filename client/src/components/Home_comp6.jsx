ce1dbf3447efd5c201a9b6d87bb063d6db4317b8
vivekratr <vivekratr@gmail.com>
2024-05-26 11:11:19 +0530

    Refactor Home_comp6 component to update background image styling and improve grid layout

--- client/src/components/Home_comp6.jsx
+++ client/src/components/Home_comp6.jsx
@@ -3,16 +3,16 @@ import BackgroundImageDiv from "./BGImageDiv";
 
 const Home_comp6 = () => {
   return (
-    <div className="relative">
+    <div className="relative w-full h-screen px-5">
       <BackgroundImageDiv
         classs={``}
         imageUrl={"https://i.imgur.com/skamSMs.png"}
       >
-        <div className="relative">
-          <div className="flex items-center justify-between">
-            <img src="https://i.imgur.com/NgsaSip.png" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+        <div className="relative w-full flex justify-center top-[26.7rem]">
+          <div className="flex items-center justify-between w-[450px]">
+            <img className=" cursor-pointer w-[122px] h-[40px] " src="https://i.imgur.com/NgsaSip.png" alt="" />
+            <img className=" cursor-pointer w-[50px] h-[79px] " src="https://i.imgur.com/7tEataB.png" alt="" />
+            <img className=" cursor-pointer w-[106px] h-[106px] " src="https://i.imgur.com/LHHnc78.png" alt="" />
           </div>
         </div>
       </BackgroundImageDiv>
