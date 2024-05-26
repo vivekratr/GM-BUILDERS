2581fa2e47dba3bd6f409847234afdfb0e2e74fe
vivekratr <vivekratr@gmail.com>
2024-05-26 11:26:47 +0530

    Refactor Footer component to optimize image loading and improve layout

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,14 @@ import React from "react";
 const Footer = () => {
   return <div className=" flex justify-center items-start ">
  <img src="https://i.imgur.com/ARpUAx4.png" alt="" className="w-[537px] h-[521px] object-cover" />
- <img src="https://i.imgur.com/lxaS5XJ.png" alt="" className="object-cover" />
+ <img src="https://i.imgur.com/lxaS5XJ.png" alt="" className="w-[74px] h-[99px] object-cover" />
+ <div className="w-[19.313rem] relative text-[1.125rem] capitalize font-semibold font-inter text-left inline-block text-white">
+<span className="text-[#7d7d7d]">Built by</span>
+<span className="text-[#FFD000]">{` `}</span>
+<span className="[text-decoration:underline]">#Builders</span>
+<span className="text-[#7d7d7d]"> in Hack’n Heights</span>
+</div>
+<div className="w-[22.313rem] relative text-[1.125rem] capitalize font-semibold font-inter text-white text-left inline-block">© 2024 gmBuilders | All rights reserved</div>
   </div>;
 };
 
