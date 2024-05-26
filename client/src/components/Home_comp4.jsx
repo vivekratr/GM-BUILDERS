823d0ebe7607bf06bdc855b3af6fc7d3452e728a
vivekratr <vivekratr@gmail.com>
2024-05-26 09:41:37 +0530

    Refactor Home_comp4 component to update grid layout and styling

--- client/src/components/Home_comp4.jsx
+++ client/src/components/Home_comp4.jsx
@@ -2,9 +2,13 @@
 
 const Home_comp4 = () => {
   return (
-    <div>
-      
-    </div>
+    <div
+    style={{ gridTemplateRows: `530px  auto` }}
+    className="grid grid-cols-12 pb-[150px] pt-[90px] gap-[48px] px-[43px] w-[100%] mx-auto p-1 "
+  >
+    
+
+  </div>
   )
 }
 
