df69aa29dd8991fe9f37500fbdd729e018291a45
vivekratr <vivekratr@gmail.com>
2024-05-26 23:57:43 +0530

    Refactor styling for BlogSection page

--- client/src/Pages/BlogSection.jsx
+++ client/src/Pages/BlogSection.jsx
@@ -324,6 +324,8 @@ const BlogSection = () => {
 <p className="m-0">My name is Benoit. I have been a software engineer for the past eight and a half years. I stayed at my previous (and first) company for seven and a half years, then I joined a new one in early 2022.</p>
 <p className="m-0">This article comes from a recent self-reflection on the things I wish I had started doing earlier in my career and the things I wish I had done differently.</p>
 </div>
+
+
             </div>
           </div>
         </div>
