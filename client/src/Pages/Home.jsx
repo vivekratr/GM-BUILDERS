9bde1d33daaae392cbbf6a1994011c57fffc3db4
vivekratr <vivekratr@gmail.com>
2024-05-26 08:54:53 +0530

    Refactor Home_comp2 component to add lazy loading for images

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,6 +2,7 @@
 import Navbar from "../components/Navbar";
 import Home_comp1 from "../components/Home_comp1";
 import Home_comp2 from "../components/Home_comp2";
+import Home_comp3 from "../components/Home_comp3";
 
 const Home = () => {
   return (
@@ -14,6 +15,9 @@ const Home = () => {
       </div>
       <div className="">
         <Home_comp2 />
+      </div>
+      <div>
+        <Home_comp3/>
       </div>
     </>
   );
---
+++ client/src/components/Home_comp3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home_comp3 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home_comp3
