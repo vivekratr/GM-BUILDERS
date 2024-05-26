d7d89f490329ba4b9f0366d049fe4aa834b42f14
vivekratr <vivekratr@gmail.com>
2024-05-26 11:15:23 +0530

    Refactor Home_comp6 component to optimize image loading and improve grid layout

--- client/src/components/Home_comp6.jsx
+++ client/src/components/Home_comp6.jsx
@@ -11,11 +11,28 @@ const Home_comp6 = () => {
         <div className="relative w-full flex justify-center top-[26.7rem]">
           <div className="flex items-center justify-between w-[450px]">
             <a target="_blank" href="https://chain.link/">
-            <img className=" cursor-pointer w-[122px] h-[40px] " src="https://i.imgur.com/NgsaSip.png" alt="" />
+              <img
+                className=" cursor-pointer w-[122px] h-[40px] "
+                src="https://i.imgur.com/NgsaSip.png"
+                alt=""
+              />
             </a>
-            <a target="_blank" href="https://plurality.network/">  <img className=" cursor-pointer w-[50px] h-[79px] " src="https://i.imgur.com/7tEataB.png" alt="" />
-          </a> <a target="_blank" href="https://thegraph.com/"><img className=" cursor-pointer w-[106px] h-[106px] " src="https://i.imgur.com/LHHnc78.png" alt="" />
-         </a>  </div>
+            <a target="_blank" href="https://plurality.network/">
+              {" "}
+              <img
+                className=" cursor-pointer w-[50px] h-[79px] "
+                src="https://i.imgur.com/7tEataB.png"
+                alt=""
+              />
+            </a>{" "}
+            <a target="_blank" href="https://thegraph.com/">
+              <img
+                className=" cursor-pointer w-[106px] h-[106px] "
+                src="https://i.imgur.com/LHHnc78.png"
+                alt=""
+              />
+            </a>{" "}
+          </div>
         </div>
       </BackgroundImageDiv>
     </div>
