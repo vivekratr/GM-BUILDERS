ecee70d20dbf832a92ff18be06597e18f0245af4
vivekratr <vivekratr@gmail.com>
2024-05-26 20:57:33 +0530

    Refactor Footer component to remove unused code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { BlogProvider } from "./context/ContextProvider";
