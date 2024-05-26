94da2e97c0809507670216c5e11bada0fcf5e050
vivekratr <vivekratr@gmail.com>
2024-05-26 07:38:24 +0530

    chore: Update Home component to include Home_comp1 and Home_comp2

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,22 +1,22 @@
 // import React from 'react'
-import Navbar from '../components/Navbar'
-import Home_comp1 from '../components/Home_comp1'
-import Home_comp2 from '../components/Home_comp2'
+import Navbar from "../components/Navbar";
+import Home_comp1 from "../components/Home_comp1";
+import Home_comp2 from "../components/Home_comp2";
 
 const Home = () => {
   return (
     <>
-    <div>
-      <Navbar/>
-    </div>
-    <div>
-    <Home_comp1/>
-    </div>
-    <div>
-      <Home_comp2/>
-    </div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Home_comp1 />
+      </div>
+      <div>
+        <Home_comp2 />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
