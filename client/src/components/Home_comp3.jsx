6adc2da6fc4927c6e9a1a8042de1609e2724651f
vivekratr <vivekratr@gmail.com>
2024-05-26 09:00:25 +0530

    Refactor Home_comp3 component to update banner text and styling

--- client/src/components/Home_comp3.jsx
+++ client/src/components/Home_comp3.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home_comp3 = () => {
   return (
     <div className='bg-black w-full py-[5.9375rem] flex-col items-start justify-center'>
-      <b className="w-[52.313rem] relative text-[5.125rem] mx-auto capitalize pb-2 inline-block font-inria-serif text-white text-left">Write, Influence, Earn</b>
-      <b className="w-[27.063rem] relative text-[1.625rem] mx-auto capitalize inline-block font-inria-serif text-[#969696] text-left">Get Paid for Your Articles and blogs</b>
+      <b className="w-[52.313rem] relative text-[5.125rem] mx-auto capitalize pb-2 block font-inria-serif text-white text-center ">Write, Influence, Earn</b>
+      <b className="w-[27.063rem] relative text-[1.625rem] mx-auto capitalize pb-[30px]block font-inria-serif text-[#969696] text-left">Get Paid for Your Articles and blogs</b>
     </div>
   )
 }
