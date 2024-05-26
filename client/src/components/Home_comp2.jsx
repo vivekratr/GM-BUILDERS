27a41cf6dac66f486741cdb4339633df1fd001e2
vivekratr <vivekratr@gmail.com>
2024-05-26 08:43:16 +0530

    Refactor Home_comp2 component formatting and update background image

--- client/src/components/Home_comp2.jsx
+++ client/src/components/Home_comp2.jsx
@@ -69,7 +69,7 @@ const Home_comp2 = () => {
         {/* 2nd col */}
 
         <div className="col-span-4 relative">
-        <BackgroundImageDiv imageUrl="https://i.imgur.com/p1CFttI.png">
+        <BackgroundImageDiv imageUrl="https://i.imgur.com/0vAeccP.png">
             <div className="flex flex-col  items-start justify-end h-full p-3">
               <div className="w-[80px] relative rounded-[47px] bg-[#FFFFFF00] box-border h-[1.438rem] text-left text-[1rem] text-white font-inter border-[0.5px] border-solid border-white">
                 <div className="flex items-center justify-center capitalize font-medium">
@@ -96,7 +96,7 @@ const Home_comp2 = () => {
         {/* 3rd row */}
 
         <div className="col-span-2 relative">
-        <BackgroundImageDiv imageUrl="https://i.imgur.com/p1CFttI.png">
+        <BackgroundImageDiv imageUrl="https://i.imgur.com/B8HPy6z.png">
             <div className="flex flex-col  items-start justify-end h-full p-3">
               <div className="w-[80px] rounded-[47px] bg-[#FFFFFF00] box-border h-[1.438rem] text-left text-[1rem] text-white font-inter border-[0.5px] border-solid border-white">
                 <div className="flex items-center justify-center capitalize ">
