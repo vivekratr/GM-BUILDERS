d39d0a3a2a0f56e6408aaf27f38b347f946743c6
vivekratr <vivekratr@gmail.com>
2024-05-26 11:14:19 +0530

    Refactor Home_comp6 component to update background image styling and improve grid layout

--- client/src/components/Home_comp6.jsx
+++ client/src/components/Home_comp6.jsx
@@ -10,9 +10,11 @@ const Home_comp6 = () => {
       >
         <div className="relative w-full flex justify-center top-[26.7rem]">
           <div className="flex items-center justify-between w-[450px]">
+            <a target="_blank" href="https://chain.link/">
             <img className=" cursor-pointer w-[122px] h-[40px] " src="https://i.imgur.com/NgsaSip.png" alt="" />
-            <img className=" cursor-pointer w-[50px] h-[79px] " src="https://i.imgur.com/7tEataB.png" alt="" />
-            <img className=" cursor-pointer w-[106px] h-[106px] " src="https://i.imgur.com/LHHnc78.png" alt="" />
+            </a>
+            <a href="https://plurality.network/">  <img className=" cursor-pointer w-[50px] h-[79px] " src="https://i.imgur.com/7tEataB.png" alt="" />
+          </a>  <img className=" cursor-pointer w-[106px] h-[106px] " src="https://i.imgur.com/LHHnc78.png" alt="" />
           </div>
         </div>
       </BackgroundImageDiv>
