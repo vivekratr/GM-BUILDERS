2f6b1d2d8c0b25cacd6b4f944d6318510b92890c
vivekratr <vivekratr@gmail.com>
2024-05-26 08:15:42 +0530

    chore: Update BGImageDiv component to include children prop and add absolute positioning

--- client/src/components/BGImageDiv.jsx
+++ client/src/components/BGImageDiv.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const BackgroundImageDiv = ({ imageUrl, children }) => {
   const divStyle = {
     backgroundImage: `url(${imageUrl})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    width: '100%',
-    height: '100%',
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    width: "100%",
+    height: "100%",
   };
 
   return (
