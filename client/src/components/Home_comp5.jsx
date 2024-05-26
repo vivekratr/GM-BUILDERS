00c579bdc30786c2771b9e161843e003c9db7e49
vivekratr <vivekratr@gmail.com>
2024-05-26 10:38:59 +0530

    Refactor Home_comp5 component to update background image styling and improve grid layout

--- client/src/components/Home_comp5.jsx
+++ client/src/components/Home_comp5.jsx
@@ -5,7 +5,10 @@ import BackgroundImageDiv from "./BGImageDiv";
 const Home_comp5 = () => {
   return <div className=" bg-black px-11 mx-auto w-full">
     <BackgroundImageDiv classs={` flex items-start justify-center p-[30px]`} imageUrl={``} >
-
+    <div className="w-full relative box-border h-[3.625rem] overflow-hidden text-left text-[1rem] text-dimgray font-inter border-[0.5px] border-solid border-white">
+<input className="absolute top-[1.188rem] left-[1rem] lowercase">enter your email</input>
+<img className="absolute top-[1.438rem] left-[11.438rem] w-[1.656rem] h-[0.781rem]" alt="" src="Group 1.svg" />
+</div>
     </BackgroundImageDiv>
 
   </div>;
