fe27ddf855bfdafbd53ed9488fdd944013149373
vivekratr <vivekratr@gmail.com>
2024-05-26 09:45:10 +0530

    Refactor Home_comp4 component to improve grid layout and styling

--- client/src/components/Home_comp4.jsx
+++ client/src/components/Home_comp4.jsx
@@ -1,12 +1,33 @@
 // import React from 'react'
 
+import BackgroundImageDiv from "./BGImageDiv"
+
 const Home_comp4 = () => {
   return (
     <div
     style={{ gridTemplateRows: `530px  auto` }}
     className="grid grid-cols-12 pb-[150px] pt-[90px] gap-[48px] px-[43px] w-[100%] mx-auto p-1 "
   >
-    
+    {/* 1st column */}
+    <div className="relative col-span-4">
+        <BackgroundImageDiv imageUrl={``}>
+        <div className="flex flex-col justify-start items-start p-[29px]">
+        <div className="flex">
+            <img src="" alt="" />
+            <div className="w-[9.813rem] relative text-[1.188rem] capitalize font-inter text-[#7C7C7C] text-left inline-block">recommendation</div>
+        </div>
+        </div>
+        </BackgroundImageDiv>
+
+    </div>
+    {/* 2nd column */}
+    <div className="relative col-span-4">
+
+    </div>
+    {/* 3rd column */}
+    <div className="relative col-span-4">
+
+    </div>
 
   </div>
   )
