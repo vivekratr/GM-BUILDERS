1c3a03a0b4e3bc07b71fcb37e475496f281c8051
vivekratr <vivekratr@gmail.com>
2024-05-26 07:38:33 +0530

    chore: Update Home_comp2 component to fix formatting and import statements

--- client/src/components/Home_comp2.jsx
+++ client/src/components/Home_comp2.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Home_comp2 = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default Home_comp2
+export default Home_comp2;
