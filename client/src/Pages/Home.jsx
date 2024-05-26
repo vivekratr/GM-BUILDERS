2b89a72bfa3c3ae554a8c1110290e7a552daff18
vivekratr <vivekratr@gmail.com>
2024-05-26 09:35:43 +0530

    feat: Add Home_comp4 component to Home page

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Home_comp1 from "../components/Home_comp1";
 import Home_comp2 from "../components/Home_comp2";
 import Home_comp3 from "../components/Home_comp3";
+import Home_comp4 from "../components/Home_comp4";
 
 const Home = () => {
   return (
@@ -18,6 +19,9 @@ const Home = () => {
       </div>
       <div>
         <Home_comp3 />
+      </div>
+      <div>
+        <Home_comp4/>
       </div>
     </>
   );
---
+++ client/src/components/Home_comp4.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home_comp4 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home_comp4
