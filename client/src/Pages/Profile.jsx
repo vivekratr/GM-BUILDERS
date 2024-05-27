d4efc5ac95fa0fb62869e510aa8a4459bf61fa00
vivekratr <vivekratr@gmail.com>
2024-05-27 10:55:05 +0530

    Refactor Profile component to add flex layout for name and username

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -14,7 +14,13 @@ const Profile = () => {
     <div className="col-span-5">
     <img src="https://i.imgur.com/TP6GXuh.png"  className=' w-full object-contain ' alt="" />
     </div>
- <div className="col-span-7">
+ <div className="col-span-7 flex flex-col">
+    <div className="flex">
+        <div className="flex flex-col">
+        <b className="w-[10.75rem] relative text-[1.625rem] inline-block font-inter text-white text-left">Aditya Kumar</b>
+        <div className="w-[10.125rem] relative text-[1.25rem] font-inter text-[#757575] text-left inline-block">@adityakumar.zk</div>
+        </div>
+    </div>
         
     </div>
 </div>
