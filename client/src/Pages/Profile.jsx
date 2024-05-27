3828ccc468248fcfd90bb6c6ed4cd965cdf21634
vivekratr <vivekratr@gmail.com>
2024-05-27 10:56:18 +0530

    Refactor Profile component to add "Edit Profile" button

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -14,12 +14,15 @@ const Profile = () => {
     <div className="col-span-5">
     <img src="https://i.imgur.com/TP6GXuh.png"  className=' w-full object-contain ' alt="" />
     </div>
- <div className="col-span-7 flex flex-col">
+ <div className="col-span-7 flex flex-col gap-4">
     <div className="flex">
         <div className="flex flex-col">
         <b className="w-[10.75rem] relative text-[1.625rem] inline-block font-inter text-white text-left">Aditya Kumar</b>
         <div className="w-[10.125rem] relative text-[1.25rem] font-inter text-[#757575] text-left inline-block">@adityakumar.zk</div>
         </div>
+        <div className="w-[125px] relative rounded-[40px] bg-gray box-border h-[2.563rem] overflow-hidden text-left text-[1rem] text-white font-inter border-[1px] border-solid border-darkslategray">
+<div className="absolute top-[0.688rem] left-[1.313rem] font-medium">Edit Profile</div>
+</div>
     </div>
         
     </div>
