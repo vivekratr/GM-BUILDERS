8b7a4600422526eb305e10d40e4bc8d760235911
vivekratr <vivekratr@gmail.com>
2024-05-28 01:41:43 +0530

    chore: Refactor PinaxSubstream component to update imports, fix headers, and improve code formatting

--- client/src/components/PinaxSubstream.jsx
+++ client/src/components/PinaxSubstream.jsx
@@ -4,12 +4,21 @@ import { BlockEmitter, createDefaultTransport } from "@substreams/node";
 
 const { MANIFEST, SUBSTREAMS_URL, SUBSTREAMS_API_KEY } = process.env;
 
-const substreamPackage = await readPackage({MANIFEST});
+const substreamPackage = await readPackage({ MANIFEST });
 
 const headers = new Headers({ "User-Agent": "@substreams/node”" });
 const registry = createRegistry(substreamPackage);
-const transport = createDefaultTransport({SUBSTREAMS_URL}, {SUBSTREAMS_API_KEY}, registry, headers);
-const request = createRequest({substreamPackage, outputModule: "graph_out", startBlockNum: -1});
+const transport = createDefaultTransport(
+  { SUBSTREAMS_URL },
+  { SUBSTREAMS_API_KEY },
+  registry,
+  headers
+);
+const request = createRequest({
+  substreamPackage,
+  outputModule: "graph_out",
+  startBlockNum: -1,
+});
 
 const emitter = new BlockEmitter(transport, request, registry);
 
@@ -24,4 +33,4 @@ emitter.on("anyMessage", (message, cursor, clock) => {
 });
 
 await emitter.start();
-console.log("✅ Done")
+console.log("✅ Done");
