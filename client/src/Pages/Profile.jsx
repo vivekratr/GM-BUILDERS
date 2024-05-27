c3f4a78ea80da098ebdf1ae6fe2c77916cd5dea0
vivekratr <vivekratr@gmail.com>
2024-05-27 11:02:24 +0530

    Refactor Profile component to improve layout and lazy load profile image

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -7,7 +7,7 @@ const Profile = () => {
       <div className="mb-[54px]">
         <Navbar />
       </div>
-      <div className="w-[80%] mx-auto relative rounded-[14px] bg-[#1a1a1a] pt-[31px] pl-[34px] pb-9 pr-4 grid grid-cols-12 h-[18.563rem] overflow-hidden text-left text-[1rem] text-white font-inter ">
+      <div className="w-[80%] mx-auto relative rounded-[14px] bg-[#1a1a1a] pt-[31px] pl-[34px] pb-9 pr-4 grid grid-cols-12  overflow-hidden text-left text-[1rem] text-white font-inter ">
         <div className=" col-span-7">
           <div className="w-[100%] grid grid-cols-12 gap-6 ">
             <div className="col-span-5">
