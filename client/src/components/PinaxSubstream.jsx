61d44f4c84450b0173d70d0b2543e483680f727a
vivekratr <vivekratr@gmail.com>
2024-05-28 01:41:32 +0530

    Refactor PinaxSubstream component to update imports and fix headers

--- client/src/components/PinaxSubstream.jsx
+++ client/src/components/PinaxSubstream.jsx
@@ -1,33 +1,27 @@
-import { createRegistry, createRequest } from “@substreams/core”;
-import { readPackage } from “@substreams/manifest”;
-import { BlockEmitter, createDefaultTransport } from “@substreams/node”;
+import { createRegistry, createRequest } from "@substreams/core";
+import { readPackage } from "@substreams/manifest";
+import { BlockEmitter, createDefaultTransport } from "@substreams/node";
 
 const { MANIFEST, SUBSTREAMS_URL, SUBSTREAMS_API_KEY } = process.env;
 
-// Read Substream
 const substreamPackage = await readPackage({MANIFEST});
 
-// Connect Transport
-const headers = new Headers({ “User-Agent”: “@substreams/node” });
+const headers = new Headers({ "User-Agent": "@substreams/node”" });
 const registry = createRegistry(substreamPackage);
 const transport = createDefaultTransport({SUBSTREAMS_URL}, {SUBSTREAMS_API_KEY}, registry, headers);
-const request = createRequest({substreamPackage, outputModule: “graph_out”, startBlockNum: -1});
+const request = createRequest({substreamPackage, outputModule: "graph_out", startBlockNum: -1});
 
-// NodeJS Events
 const emitter = new BlockEmitter(transport, request, registry);
 
-// Session Trace ID
 emitter.on("session", (session) => {
   console.dir(session);
 });
 
-// Stream Blocks
 emitter.on("anyMessage", (message, cursor, clock) => {
   console.dir(message);
   console.dir(cursor);
   console.dir(clock);
 });
 
-// Start Emitter
 await emitter.start();
 console.log("✅ Done")
