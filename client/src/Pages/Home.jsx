6e28046d1365324995e2c9f41289084f93904d7f
vivekratr <vivekratr@gmail.com>
2024-05-27 19:11:37 +0530

    Refactor Home component to fix useContext import and add missing state variable

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -191,9 +191,11 @@ const Home = () => {
                   Home
                 </a>
               </li>
-              <li onClick={()=>{
-                navigate('/blogs')
-              }}>
+              <li
+                onClick={() => {
+                  navigate("/blogs");
+                }}
+              >
                 <a
                   href="#"
                   className="text-[1.125rem] capitalize font-medium font-inter text-white text-left inline-block"
